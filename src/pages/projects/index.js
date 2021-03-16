22e79d10f401697b71b1925c28d32c54e3c770c6
Jonathan Shitrit <Jonathanshitrit8@gmail.com>
2021-03-16 17:51:51 -0400

    completed portfolio

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -5,32 +5,32 @@ import * as styles from '../../styles/projects.module.css'
 import { GatsbyImage } from "gatsby-plugin-image";
 
 export default function Projects({ data }) {
-    console.log(data)
-    const projects = data.projects.nodes
-    const contact = data.contact.siteMetadata.contact
+  // console.log(data)
+  const projects = data.projects.nodes
+  const contact = data.contact.siteMetadata.contact
 
-    return (
-        <Layout>
-            <div className={styles.portfolio}>
-                <h2>Portfolio</h2>
-                <h3>Projects & Websites I've Created</h3>
-                <div className={styles.projects}>
-                    {projects.map(project => (
-                        <Link
-                            to={"/project/" + project.frontmatter.slug}
-                            key={project.id}>
-                            <div>
-                                <GatsbyImage image={project.frontmatter.thumb.childImageSharp.gatsbyImageData} alt="design-image" />
-                                <h3>{project.frontmatter.title}</h3>
-                                <p>{project.frontmatter.stack}</p>
-                            </div>
-                        </Link>
-                    ))}
-                </div>
-                <p>Like what you see? Email me at {contact}</p>
-            </div>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <div className={styles.portfolio}>
+        <h2>Portfolio</h2>
+        <h3>Projects & Websites I've Created</h3>
+        <div className={styles.projects}>
+          {projects.map(project => (
+            <Link
+              to={"/projects/" + project.frontmatter.slug}
+              key={project.id}>
+              <div>
+                <GatsbyImage image={project.frontmatter.thumb.childImageSharp.gatsbyImageData} alt="design-image" />
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+        <p>Like what you see? Email me at {contact}</p>
+      </div>
+    </Layout>
+  )
 }
 
 export const query = graphql`
