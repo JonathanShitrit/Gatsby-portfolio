63dc198965573e7c1735a9c31b3d4180196ad488
Jonathan Shitrit <Jonathanshitrit8@gmail.com>
2021-03-16 16:16:11 -0400

    completed lesson 17

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from "../components/Layout"
+
+export default function About() {
+    return (
+        <Layout>
+            <div>
+                <h1>About Page</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Quis hendrerit dolor magna eget est lorem ipsum. Et netus et malesuada fames ac turpis egestas integer eget.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Quis hendrerit dolor magna eget est lorem ipsum. Et netus et malesuada fames ac turpis egestas integer eget.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Quis hendrerit dolor magna eget est lorem ipsum. Et netus et malesuada fames ac turpis egestas integer eget.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Quis hendrerit dolor magna eget est lorem ipsum. Et netus et malesuada fames ac turpis egestas integer eget.</p>
+            </div>
+        </Layout>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,31 @@
+import { graphql, Link } from "gatsby"
 import React from "react"
+import Layout from "../components/Layout"
+import * as styles from '../styles/home.module.css'
+import { GatsbyImage } from "gatsby-plugin-image";
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data }) {
+  return (
+    <Layout>
+      <section className={styles.header}>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop & Deploy</h3>
+          <p>Full-Stack developer based in New York City</p>
+          <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
+        </div>
+        <GatsbyImage image={data.file.childImageSharp.gatsbyImageData} alt="design-image" />
+      </section>
+    </Layout>
+  );
 }
+
+export const query = graphql`
+query Banner {
+  file(relativePath: {eq: "banner.png"}) {
+    childImageSharp {
+      gatsbyImageData
+    }
+  }
+}
+`
